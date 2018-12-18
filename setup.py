dc24b779bfa1660b1eff3e2f7094fd2656d0db95
Charles Watkins <charles@titandws.com>
2018-12-18 14:06:17 -0500

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.extension import Extension
 
 setup(
     name='vaultfly',
-    version='1.0.16',
+    version='1.0.17',
     packages=['vaultfly'],
     include_package_data=True,
     url='https://github.com/chris17453/vaultfly/',
